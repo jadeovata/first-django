74d1f1887e1f08ff969dd5c34e69e9b3bcead3d3
Jade Ovata <jadeovata@outlook.com>
2018-08-07 10:53:06 -0700

    add views and templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.story_list,name='story_list'),
+    path('story/<int:pk>/',views.story_detail,name='story_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Story
 
@@ -6,3 +6,7 @@ from .models import Story
 def story_list(request):
     stories = Story.objects.order_by('created')
     return render(request,'blog/story_list.html',{'stories':stories})
+
+def story_detail(request, pk):
+    story = get_object_or_404(Story, pk=pk)
+    return render(request, 'blog/story_detail.html',{'story': story})
