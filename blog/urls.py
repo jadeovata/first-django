6a217eb4f7a1dccf4f83275a3e48c53239152d11
Jade Ovata <jadeovata@outlook.com>
2018-08-07 13:06:33 -0700

    updated views

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.story_list,name='story_list'),
-    path('story/<int:pk>/',views.story_detail,name='story_detail')
+    path('story/<int:pk>/',views.story_detail,name='story_detail'),
+    path('story/new',views.story_new,name='story_new'),
+    path('story/<int:pk>/edit/',views.story_edit, name='story_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
 from .models import Story
+from .forms import StoryForm
 
 # Create your views here.
 def story_list(request):
@@ -10,3 +12,31 @@ def story_list(request):
 def story_detail(request, pk):
     story = get_object_or_404(Story, pk=pk)
     return render(request, 'blog/story_detail.html',{'story': story})
+
+def story_new(request):
+    if request.method == "POST":
+        ##
+        form = StoryForm(request.POST)
+        if form.is_valid():
+            story = form.save(commit=False)
+            story.author = request.user
+            story.published = timezone.now()
+            story.save()
+            return redirect('story_detail', pk=story.pk)
+    else:
+        form = StoryForm()
+    return render(request, 'blog/story_edit.html',{'form':form})
+
+def story_edit(request, pk):
+    story = get_object_or_404(Story, pk=pk)
+    if request.method == "POST":
+        form = StoryForm(request.POST, instance=story)
+        if form.is_valid():
+            story = form.save(commit=False)
+            story.author = request.user
+            story.published = timezone.now()
+            story.save()
+            return redirect('story_detail', pk=story.pk)
+    else:
+        form = StoryForm(instance=story)
+    return render(request,'blog/story_edit.html',{'form':form})
