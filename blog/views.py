72eed682f2f09d14c5b3b92a55575d177de2de27
Jade Ovata <jadeovata@outlook.com>
2018-08-06 23:38:36 -0700

    modify template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Story
 
 # Create your views here.
 def story_list(request):
-    return render(request,'blog/story_list.html',{})
+    stories = Story.objects.order_by('created')
+    return render(request,'blog/story_list.html',{'stories':stories})
